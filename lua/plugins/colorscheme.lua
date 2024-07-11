03e0de0c119196ed387f8de75ac437166a354c3a
Maverick <tayqinyuan@gmail.com>
2024-07-11 18:40:13 +0100

    change colorscheme to cyberdream

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,62 +1,25 @@
 return {
-  "rebelot/kanagawa.nvim",
+  "scottmckendry/cyberdream.nvim",
   lazy = false,
+  dev = true,
   priority = 1000,
   opts = {
-    undercurl = true,
-    commentStyle = { italic = true },
-    keywordStyle = { italic = true },
-    statementStyle = { bold = true },
-    transparent = false,
-    dimInactive = false,
-    terminalColors = true,
-    colors = { -- add/modify theme and palette colors
-      palette = {},
-      theme = {
-        wave = {},
-        lotus = {},
-        dragon = {},
-        all = {
-          ui = {
-            bg_gutter = "none",
-          },
-        },
-      },
-    },
-    overrides = function(colors)
-      local theme = colors.theme
-      return {
-        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-        PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-        PmenuSbar = { bg = theme.ui.bg_m1 },
-        PmenuThumb = { bg = theme.ui.bg_p2 },
-        TelescopeTitle = { fg = theme.ui.special, bold = true },
-        TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-        TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-        TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-        TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-        TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-        TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-        Normloat = { bg = "none" },
-        FloatBorder = { bg = "none" },
-        FloatTitle = { bg = "none" },
-
-        -- Save an hlgroup with dark background and dimmed foreground
-        -- so that you can use it where your still want darker windows.
-        -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-        -- Popular plugins that open floats will link to NormalFloat by default;
-        -- set their background accordingly if you wish to keep them dark and borderless
-        LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-        MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-      }
-    end,
-
-    theme = "wave", -- Load "wave" theme when 'background' option is not set
-    background = { -- map the value of 'background' option to a theme
-      dark = "wave", -- try "dragon" !
-      light = "lotus",
+    transparent = true,
+    italic_comments = true,
+    hide_fillchars = false,
+    borderless_telescope = { border = false, style = "flat" },
+    terminal_colors = false,
+    theme = {
+      variant = "auto", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
+      overrides = function(colours)
+        return {
+          TelescopePromptPrefix = { fg = colours.blue },
+          TelescopeMatching = { fg = colours.cyan },
+          TelescopeResultsTitle = { fg = colours.blue },
+          TelescopePromptCounter = { fg = colours.cyan },
+          TelescopePromptTitle = { fg = colours.bg, bg = colours.blue, bold = true },
+        }
+      end,
     },
   },
 }
